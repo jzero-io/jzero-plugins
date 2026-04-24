e98934b87fd22e6f04fc640dc33a07f86a551320
0xjaron <jaron@jaronnie.com>
2026-04-24 16:16:29 +0800

    feat(jzero-hello): update

--- jzero-hello/go.mod
+++ jzero-hello/go.mod
@@ -5,6 +5,7 @@ go 1.25.0
 require (
 	github.com/jzero-io/jzero/cmd/jzero v1.3.1-0.20260403033439-dd13b14d07c4
 	github.com/spf13/cobra v1.10.1
+	github.com/zeromicro/ddl-parser v1.0.5
 )
 
 require (
@@ -34,7 +35,6 @@ require (
 	github.com/spf13/viper v1.21.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
-	github.com/zeromicro/ddl-parser v1.0.5 // indirect
 	github.com/zeromicro/go-zero v1.10.0 // indirect
 	github.com/zeromicro/go-zero/tools/goctl v1.9.2 // indirect
 	go.opentelemetry.io/otel v1.40.0 // indirect
--- jzero-hello/main.go
+++ jzero-hello/main.go
@@ -9,14 +9,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/jzero-io/jzero/cmd/jzero/pkg/plugin"
 	"github.com/spf13/cobra"
+	ddlparser "github.com/zeromicro/ddl-parser/parser"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "jzero-hello",
-	Short: "A simple hello world plugin for jzero",
+	Short: "A simple hello plugin for jzero",
 	Long: `This is an example plugin for jzero that demonstrates
 how to create and structure a jzero plugin.
 
@@ -34,23 +36,34 @@ var descCmd = &cobra.Command{
 		}
 
 		if p.Desc.Api.SpecMap != nil {
-			fmt.Printf("get %d api file\n", len(p.Desc.Api.SpecMap))
-			for file := range p.Desc.Api.SpecMap {
+			for file, spec := range p.Desc.Api.SpecMap {
 				fmt.Printf("api file: %s\n", file)
+				for _, group := range spec.Service.Groups {
+					fmt.Printf("  group: %s\n", group.GetAnnotation("group"))
+					for _, route := range group.Routes {
+						fmt.Printf("    %s\n", fmt.Sprintf("%s:%s%s", route.Method, group.GetAnnotation("prefix"), route.Path))
+					}
+				}
 			}
 		}
 
 		if p.Desc.Proto.SpecMap != nil {
-			fmt.Printf("get %d proto file\n", len(p.Desc.Proto.SpecMap))
-			for file := range p.Desc.Proto.SpecMap {
+			for file, spec := range p.Desc.Proto.SpecMap {
 				fmt.Printf("proto file: %s\n", file)
+				for _, service := range spec.Service {
+					for _, rpc := range service.RPC {
+						fmt.Printf("  %s.%s\n", service.Name, rpc.Name)
+					}
+				}
 			}
 		}
 
 		if p.Desc.Model.SpecMap != nil {
-			fmt.Printf("get %d model file\n", len(p.Desc.Model.SpecMap))
-			for file := range p.Desc.Model.SpecMap {
-				fmt.Printf("model file: %s\n", file)
+			for file, spec := range p.Desc.Model.SpecMap {
+				fmt.Printf("sql file: %s\n", file)
+				for _, columns := range spec.Columns {
+					fmt.Printf("  %s(%s)\n", columns.Name, formatDataType(columns.DataType))
+				}
 			}
 		}
 
@@ -60,6 +73,89 @@ var descCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(descCmd)
+}
+
+var dataTypeNames = map[int]string{
+	ddlparser.LongVarBinary:      "LongVarBinary",
+	ddlparser.LongVarChar:        "LongVarChar",
+	ddlparser.GeometryCollection: "GeometryCollection",
+	ddlparser.GeomCollection:     "GeomCollection",
+	ddlparser.LineString:         "LineString",
+	ddlparser.MultiLineString:    "MultiLineString",
+	ddlparser.MultiPoint:         "MultiPoint",
+	ddlparser.MultiPolygon:       "MultiPolygon",
+	ddlparser.Point:              "Point",
+	ddlparser.Polygon:            "Polygon",
+	ddlparser.Json:               "Json",
+	ddlparser.Geometry:           "Geometry",
+	ddlparser.Enum:               "Enum",
+	ddlparser.Set:                "Set",
+	ddlparser.Bit:                "Bit",
+	ddlparser.Time:               "Time",
+	ddlparser.Timestamp:          "Timestamp",
+	ddlparser.DateTime:           "DateTime",
+	ddlparser.Binary:             "Binary",
+	ddlparser.VarBinary:          "VarBinary",
+	ddlparser.Blob:               "Blob",
+	ddlparser.Year:               "Year",
+	ddlparser.Decimal:            "Decimal",
+	ddlparser.Dec:                "Dec",
+	ddlparser.Fixed:              "Fixed",
+	ddlparser.Numeric:            "Numeric",
+	ddlparser.Float:              "Float",
+	ddlparser.Float4:             "Float4",
+	ddlparser.Float8:             "Float8",
+	ddlparser.Double:             "Double",
+	ddlparser.Real:               "Real",
+	ddlparser.TinyInt:            "TinyInt",
+	ddlparser.SmallInt:           "SmallInt",
+	ddlparser.MediumInt:          "MediumInt",
+	ddlparser.Int:                "Int",
+	ddlparser.Integer:            "Integer",
+	ddlparser.BigInt:             "BigInt",
+	ddlparser.MiddleInt:          "MiddleInt",
+	ddlparser.Int1:               "Int1",
+	ddlparser.Int2:               "Int2",
+	ddlparser.Int3:               "Int3",
+	ddlparser.Int4:               "Int4",
+	ddlparser.Int8:               "Int8",
+	ddlparser.Date:               "Date",
+	ddlparser.TinyBlob:           "TinyBlob",
+	ddlparser.MediumBlob:         "MediumBlob",
+	ddlparser.LongBlob:           "LongBlob",
+	ddlparser.Bool:               "Bool",
+	ddlparser.Boolean:            "Boolean",
+	ddlparser.Serial:             "Serial",
+	ddlparser.NVarChar:           "NVarChar",
+	ddlparser.NChar:              "NChar",
+	ddlparser.Char:               "Char",
+	ddlparser.Character:          "Character",
+	ddlparser.VarChar:            "VarChar",
+	ddlparser.TinyText:           "TinyText",
+	ddlparser.Text:               "Text",
+	ddlparser.MediumText:         "MediumText",
+	ddlparser.LongText:           "LongText",
+}
+
+func formatDataType(dt ddlparser.DataType) string {
+	if dt == nil {
+		return "Unknown"
+	}
+
+	name, ok := dataTypeNames[dt.Type()]
+	if !ok {
+		name = fmt.Sprintf("Unknown(%d)", dt.Type())
+	}
+
+	if values := dt.Value(); len(values) > 0 {
+		name = fmt.Sprintf("%s[%s]", name, strings.Join(values, ", "))
+	}
+
+	if dt.Unsigned() {
+		name += " Unsigned"
+	}
+
+	return name
 }
 
 func main() {
